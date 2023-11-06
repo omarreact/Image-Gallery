e64d8ad8ee33c1b6a92d0315dd3f606203e4c07a
Faruk Khan <fk8187990@gmail.com>
2023-11-06 20:13:16 +0600

    Updates

---
+++ precache-manifest.a32b90ac2f7f658b84afc2a1cb7dc0da.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffb38510af562e76065913add7090d1",
+    "url": "/Image-Gallery/index.html"
+  },
+  {
+    "revision": "b261cb232d979483cb63",
+    "url": "/Image-Gallery/static/css/2.0b4032cb.chunk.css"
+  },
+  {
+    "revision": "5b157e9455f3bac87a2e",
+    "url": "/Image-Gallery/static/css/main.b4226b02.chunk.css"
+  },
+  {
+    "revision": "b261cb232d979483cb63",
+    "url": "/Image-Gallery/static/js/2.3894cb7e.chunk.js"
+  },
+  {
+    "revision": "9fd8ac69f8779f68b807eb06f9423c10",
+    "url": "/Image-Gallery/static/js/2.3894cb7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b157e9455f3bac87a2e",
+    "url": "/Image-Gallery/static/js/main.ccdcfc24.chunk.js"
+  },
+  {
+    "revision": "ab2b5334a397e346f7e2",
+    "url": "/Image-Gallery/static/js/runtime-main.131e7fec.js"
+  },
+  {
+    "revision": "779de01ea3080cb291ed05bb13c18760",
+    "url": "/Image-Gallery/static/media/image-1.779de01e.webp"
+  },
+  {
+    "revision": "eedaad5276747d325de9b00dd9e131f3",
+    "url": "/Image-Gallery/static/media/image-10.eedaad52.jpeg"
+  },
+  {
+    "revision": "6f96b73c54df37413378b1634b09e022",
+    "url": "/Image-Gallery/static/media/image-11.6f96b73c.jpeg"
+  },
+  {
+    "revision": "a94d5c371a48305d5e0917c6750aa6d1",
+    "url": "/Image-Gallery/static/media/image-2.a94d5c37.webp"
+  },
+  {
+    "revision": "7d48af120b802570a9793c4a494ce2c3",
+    "url": "/Image-Gallery/static/media/image-3.7d48af12.webp"
+  },
+  {
+    "revision": "a1f1f7a8f7c4d57247ac4d5edcbc7de0",
+    "url": "/Image-Gallery/static/media/image-4.a1f1f7a8.webp"
+  },
+  {
+    "revision": "46ae4ce187f4b38f87425370d5a7ea41",
+    "url": "/Image-Gallery/static/media/image-5.46ae4ce1.webp"
+  },
+  {
+    "revision": "3372501b29e470040acde289e7e0c5f4",
+    "url": "/Image-Gallery/static/media/image-6.3372501b.webp"
+  },
+  {
+    "revision": "33e3c94125ba3450d976e88535c45ef6",
+    "url": "/Image-Gallery/static/media/image-7.33e3c941.webp"
+  },
+  {
+    "revision": "23e4493d0db3c2929355b3498d590490",
+    "url": "/Image-Gallery/static/media/image-8.23e4493d.webp"
+  },
+  {
+    "revision": "b32cd2c68502dff0fdeeaf9bfa5e6309",
+    "url": "/Image-Gallery/static/media/image-9.b32cd2c6.webp"
+  }
+]);
